50c14ce28460de8b335ff74865f80a37fdef350d
David Pedersen <david.pdrsn@gmail.com>
2019-03-30 13:13:37 +0100

    Add test that doesn't use fragment

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ pub struct User {
 graphql_object!(User: Context |&self| {
     field country(&executor) -> FieldResult<&Country> {
         executor.look_ahead(); // <-- PANIC
+
         Ok(&self.country)
     }
 });
@@ -45,7 +46,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_something() {
+    fn with_fragment() {
         let user = User {
             country: Country { id: 1 },
         };
@@ -62,6 +63,34 @@ mod test {
 
                 fragment userFields on User {
                   country { id }
+                }
+            "#,
+            None,
+            &Schema::new(Query, EmptyMutation::new()),
+            &Variables::new(),
+            &context,
+        )
+        .unwrap();
+
+        let res = serde_json::to_value(res).expect("convert to `serde_json::Value`");
+
+        assert_eq!(res["user"]["country"]["id"].as_i64().unwrap(), 1);
+    }
+
+    #[test]
+    fn without_fragment() {
+        let user = User {
+            country: Country { id: 1 },
+        };
+
+        let context = Context { user };
+
+        let (res, _errors) = juniper::execute(
+            r#"
+                query Foo {
+                  user {
+                    country { id }
+                  }
                 }
             "#,
             None,
